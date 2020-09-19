dbdbca38bd3e8050d8d7510c215000976e4d09a0
echoi3-hw <70167495+echoi3-hw@users.noreply.github.com>
2020-09-19 13:49:58 -0700

    Update Decoder.java

--- Decoder.java
+++ Decoder.java
@@ -1,13 +1,62 @@
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
-public class Decoder {
+public class LZWDecoder {
+	//make table that has the list of string and their value
+	private ArrayList<String> table = new ArrayList<String>(); 
 	
-	public Decoder (String filename) throws FileNotFoundException
+	public LZWDecoder ()
 	{
-		Scanner reader = new Scanner (new File(filename));
-		reader.nextInt();
+	}
+	
+		
+	public void decodeText(String fileName) throws FileNotFoundException
+	{
+		try {
+		//make printwriter so we can print as we encode
+		PrintWriter pw = new PrintWriter(new FileWriter(fileName  + "encoded"));
+		Scanner reader = new Scanner (new File(fileName));
+		
+		for (int i=0; i<=127; i++)
+		{
+			table.add(""+(char)(i)); //inputs values into table that are already in the ascii table; began at 33rd character to avoid weird chars
+		}
+		int code1=0;
+		code1 = reader.nextInt();
+		
+		pw.print(table.get(code1));
+		int w = code1;
+		
+		while (reader.hasNext())
+		{
+			code1 = reader.nextInt();
+			if (code1 <= 127) // if code read in is in dictionary
+			{
+				String entry = table.get(code1);
+				pw.print(entry);
+				table.add(table.get(w) + entry.charAt(0) + "");
+				w = table.indexOf(entry);
+			}
+			else //if code is not in dictionary
+			{
+				
+				pw.print(table.get(w) + table.get(w).charAt(0)+ "");
+				table.add(table.get(w) + table.get(w).charAt(0)+ "");
+				w=table.indexOf(table.get(w) + table.get(w).charAt(0)+ "");
+			}
+		}
+		
 		
 	}
+	catch (IOException e)
+	{
+		System.out.println("error");
+	}
 
+}
 }

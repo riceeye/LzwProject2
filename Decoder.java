e794c41903033a9c7a1a328ca1f166db43b3c051
echoi3-hw <70167495+echoi3-hw@users.noreply.github.com>
2020-09-20 15:56:11 -0700

    Update Decoder.java
    added comments

--- Decoder.java
+++ Decoder.java
@@ -20,6 +20,7 @@ public class Decoder {
 		try {
 		//make printwriter so we can print as we encode
 		PrintWriter pw = new PrintWriter(new FileWriter(fileName  + "decoded"));
+		// make reader to read in integers
 		Scanner reader = new Scanner (new File(fileName));
 		
 		for (int i=0; i<=127; i++)
@@ -28,28 +29,28 @@ public class Decoder {
 		}
 
 		
-		int readIn = reader.nextInt();
+		int readIn = reader.nextInt(); // new number
 		
-		pw.print(table.get(readIn));
-		int beginning = readIn;
+		pw.print(table.get(readIn)); // prints very first char
+		int beginning = readIn; // beginning is the previous code
 		
 		while (reader.hasNext()==true)
 		{
 			
-			readIn = reader.nextInt();
+			readIn = reader.nextInt(); // read in next number
 			if (readIn <table.size()) // if code read in is in dictionary
 			{
-				String entry = table.get(readIn);
-				pw.print(entry);
-				table.add(table.get(beginning) + entry.charAt(0) + "");
-				beginning = table.indexOf(entry);
+				String entry = table.get(readIn); // find value of new code and print
+				pw.print(entry); 
+				table.add(table.get(beginning) + entry.charAt(0) + ""); // add to table the char of the last code + first letter of current code
+				beginning = table.indexOf(entry); // current code becomes previous
 			}
 			else //if code is not in dictionary
 			{
 				
-				pw.print(table.get(beginning) + table.get(beginning).charAt(0)+ "");
-				table.add(table.get(beginning) + table.get(beginning).charAt(0)+ "");
-				beginning=table.indexOf(table.get(beginning) + table.get(beginning).charAt(0)+ "");
+				pw.print(table.get(beginning) + table.get(beginning).charAt(0)+ ""); // print the value or chars of previous code + first letter of new code
+				table.add(table.get(beginning) + table.get(beginning).charAt(0)+ "");// add what you printed to table
+				beginning=table.indexOf(table.get(beginning) + table.get(beginning).charAt(0)+ ""); // this printed value becomes the previous code
 			}
 		}
 		

7b14d2f7889e55c2eb33699c43ff229c0eb791a1
skylarskylar <70194012+skylarskylar@users.noreply.github.com>
2020-09-20 23:11:56 -0700

    changed scanner to buffered reader

--- Decoder.java
+++ Decoder.java
@@ -7,40 +7,48 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 public class Decoder {
-	//make table that has the list of string and their value
+	//make table that has the list of strings and their value
 	private ArrayList<String> table = new ArrayList<String>(); 
 	
 	public Decoder ()
 	{
 	}
 	
-		
 	public void decodeText(String fileName) throws IOException
 	{
 		try {
 		//make printwriter so we can print as we encode
-		PrintWriter pw = new PrintWriter(new FileWriter(fileName  + "decoded"));
-		// make reader to read in integers
-		Scanner reader = new Scanner (new File(fileName));
+		PrintWriter pw = new PrintWriter(new FileWriter(fileName  + ".decoded"));
 		
+		//Scanner reader = new Scanner (new File(fileName)); replaced with a buffered reader
+		BufferedReader br = new BufferedReader (new FileReader(fileName));		
+		
+		
+		//inputs values into table that are already in the ascii table
 		for (int i=0; i<=127; i++)
 		{
-			table.add(""+(char)(i)); //inputs values into table that are already in the ascii table; began at 33rd character to avoid weird chars
+			table.add(""+(char)(i)); 
 		}
 
+		//makes a string to hold the number (the numbers are separated by spaces, so this variable holds the number between them) and adds chars until the number/code is complete
+			String code = "";
+
+		pw.print(table.get(0)); // prints very first char
 		
-		int readIn = reader.nextInt(); // new number
 		
-		pw.print(table.get(readIn)); // prints very first char
-		int beginning = readIn; // beginning is the previous code
-		
-		while (reader.hasNext()==true)
+		while (br.ready())
 		{
+			int i = br.read();
+			while (i != 32) {
+				code+=(""+ (char)i);
+				br.read();
+			}
 			
-			readIn = reader.nextInt(); // read in next number
-			if (readIn <table.size()) // if code read in is in dictionary
+			int num = Integer.parseInt(code);
+			int beginning = num; // beginning is the previous code
+]			if (table.contains(code)) // if code read in is in dictionary
 			{
-				String entry = table.get(readIn); // find value of new code and print
+				String entry = table.get(num); // find value of new code and print
 				pw.print(entry); 
 				table.add(table.get(beginning) + entry.charAt(0) + ""); // add to table the char of the last code + first letter of current code
 				beginning = table.indexOf(entry); // current code becomes previous

86cddd15044a8d9368e15ea7079fe495073c5508
echoi3-hw <70167495+echoi3-hw@users.noreply.github.com>
2020-09-22 14:40:03 -0700

    Update Decoder.java
    comments and changes variable names

--- Decoder.java
+++ Decoder.java
@@ -21,34 +21,35 @@ public class Decoder {
 	{
 		//make printwriter so we can print as we encode
 		PrintWriter pw = new PrintWriter(new FileWriter(fileName  + ".decoded"));
-
+		
+		//make scanner to scan in code
 		Scanner scan = new Scanner (new File (fileName));
 
-		int readIn = scan.nextInt();
-		pw.print(table.get(readIn));
-		String beg = table.get(readIn);
+		int readIn = scan.nextInt(); // takes in the very beginning of the code, variable readIn represents what's just been read in from code
+		pw.print(table.get(readIn)); // printing first string 
+		String pastEntry = table.get(readIn); // update the past entry with the current
 
-		String newEntry = "";
+		String specialEntry = ""; // used for when the code is not in the dictionary
 
 		while (scan.hasNext())
 		{
-			readIn = scan.nextInt();
+			readIn = scan.nextInt(); // read in next code (so next number between space)
 			if (table.containsKey(readIn)) // if code read in is in dictionary
 			{
-				String entry = table.get(readIn);
-				pw.print(entry);
-				table.put(table.size(), beg + entry.charAt(0) + "");
-				beg = entry;
+				String currentEntry = table.get(readIn); // string value of most recent code
+				pw.print(currentEntry); 
+				table.put(table.size(), pastEntry + currentEntry.charAt(0) + ""); // add to end of table the previous string and the first char of current code
+				pastEntry = currentEntry; // update value of past entry with current code
 			}
 			else //if code is not in dictionary
 			{
-				newEntry = (beg + beg.charAt(0)+ "");
-				pw.print(newEntry);
-				table.put(table.size(), newEntry);
-				beg=newEntry + "";
+				specialEntry = (pastEntry + pastEntry.charAt(0)+ ""); // special case: entry of readIn is the past entry + its first char
+				pw.print(specialEntry); //print the current entry
+				table.put(table.size(), specialEntry); // add entry to table
+				pastEntry=specialEntry + ""; // update value of past entry
 			}
 		}
 		scan.close();
-		pw.close();		
+		pw.close();	//save
 	}
 }
